e899d216addcabe5dad6fd42377d4240880d55ce
Robison Cris Brito <44240574+robisoncrisbrito@users.noreply.github.com>
2025-11-08 13:56:15 -0300

    Aplicativo com a tela de Lancamento desenhada, porém, nao funciona os botões de lancamento ainda. Botão da tela principal ja chama a tela de lançamento.

--- app/src/main/java/br/edu/utfpr/trocatela_posmoveis2025_2/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/trocatela_posmoveis2025_2/MainActivity.kt
@@ -1,23 +1,18 @@
 package br.edu.utfpr.trocatela_posmoveis2025_2
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 
-    fun btLancamentoOnClick(view: View) {}
+    fun btLancamentoOnClick(view: View) {
+        val intent = Intent(this, LancamentoActivity::class.java)
+        startActivity(intent)
+    }
 }
