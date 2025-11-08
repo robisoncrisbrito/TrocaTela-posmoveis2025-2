6e232e24e833dd5acdb23f0e14afb2ae1ea691a3
Robison Cris Brito <44240574+robisoncrisbrito@users.noreply.github.com>
2025-11-08 14:29:55 -0300

    Aplicativo com tela de ConfirmarActivity pronta, com a codificação do botão confirmar da tela de lançamento, e recebendo dados desta tela no ConfirmarActivity

--- app/src/main/java/br/edu/utfpr/trocatela_posmoveis2025_2/LancamentoActivity.kt
+++ app/src/main/java/br/edu/utfpr/trocatela_posmoveis2025_2/LancamentoActivity.kt
@@ -1,27 +1,47 @@
 package br.edu.utfpr.trocatela_posmoveis2025_2
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LancamentoActivity : AppCompatActivity() {
+
+    private lateinit var etCod: EditText
+    private lateinit var etQtd: EditText
+    private lateinit var etValor: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_lancamento)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        etCod = findViewById(R.id.etCod)
+        etQtd = findViewById(R.id.etQtd)
+        etValor = findViewById(R.id.etValor)
     }
 
     fun btConfirmarOnClick(view: View) {
+        val intent = Intent( this, ConfirmarActivity::class.java)
 
+        //o campo código será transferido como inteiro para mostrar como fazer a recuperação de um numérico na tela de confirmar
+        intent.putExtra("cod", etCod.text.toString().toInt() )
+        intent.putExtra("qtd", etQtd.text.toString() )
+        intent.putExtra("valor", etValor.text.toString() )
+
+        startActivity(intent)
     }
+
     fun btListarOnClick(view: View) {
 
     }
